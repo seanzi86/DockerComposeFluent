bfb0d9c08c475153165158b6c484fd112deda510
seanzi86 <seanzi@msn.com>
2026-09-23 20:28:06 +0100

    Auto-discover YAML type converters instead of registering each by hand
    New converters only need to implement IYamlTypeConverter in this assembly.

--- src/DockerComposeFluent/Serialisation/ComposeYamlSerialiser.cs
+++ src/DockerComposeFluent/Serialisation/ComposeYamlSerialiser.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using DockerComposeFluent.Models;
 using YamlDotNet.Serialization;
 
@@ -5,16 +7,28 @@ namespace DockerComposeFluent.Serialisation
 {
     internal static class ComposeYamlSerialiser
     {
-        private static readonly ISerializer Serializer = new SerializerBuilder()
-            .WithTypeConverter(new DockerComposeFileYamlConverter())
-            .WithTypeConverter(new ServiceDefinitionYamlConverter())
-            .WithTypeConverter(new NetworkDefinitionYamlConverter())
-            .WithTypeConverter(new VolumeDefinitionYamlConverter())
-            .Build();
+        private static readonly ISerializer Serialiser = CreateSerialiser();
 
         internal static string Serialise(DockerComposeFile file)
         {
-            return Serializer.Serialize(file);
+            return Serialiser.Serialize(file);
+        }
+
+        private static ISerializer CreateSerialiser()
+        {
+            SerializerBuilder builder = new SerializerBuilder();
+
+            Type[] converterTypes = typeof(ComposeYamlSerialiser).Assembly
+                .GetTypes()
+                .Where(type => type.IsClass && !type.IsAbstract && typeof(IYamlTypeConverter).IsAssignableFrom(type))
+                .ToArray();
+
+            foreach (Type converterType in converterTypes)
+            {
+                builder.WithTypeConverter((IYamlTypeConverter)Activator.CreateInstance(converterType)!);
+            }
+
+            return builder.Build();
         }
     }
 }
